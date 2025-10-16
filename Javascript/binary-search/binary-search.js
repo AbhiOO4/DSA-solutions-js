75b8990053aa831088aa127d4867abd82651cc9f
Abhinav Sreejith <abhinavsree243@gmail.com>
2025-10-16 12:37:26 +0530

    binary search

---
+++ Javascript/binary-search/binary-search.js
@@ -0,0 +1,22 @@
+var search = function(nums, target) {
+    let l = 0
+    let r = nums.length - 1
+    while (l<=r){
+        let mid = Math.floor((l + r)/2)
+        console.log(mid)
+        if (nums[mid] < target){
+            l = mid+1
+        }
+        else if( nums[mid] > target){
+            r = mid-1
+        }
+        else{
+            return mid
+        }
+    }
+    return -1  
+};
+
+let nums = [5]
+
+console.log(search(nums, 5))
