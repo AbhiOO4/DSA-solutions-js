bc52e89858668c373ca4c943c39167707e79eca7
Abhinav Sreejith <abhinavsree243@gmail.com>
2025-10-24 09:18:48 +0530

    maximum subarray

---
+++ Javascript/Array/max-subarray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0]
+    let sum = 0
+     for (let num of nums){
+        if (sum < 0){
+            sum = 0
+        }
+        sum+= num
+        maxSum = Math.max(maxSum, sum)
+     }
+     return maxSum
+};
